9fb4a131753086d8ffd4699899f6f7832448662c
Aakash Thatte <aathatte2002@gmail.com>
2024-02-05 23:54:11 +0530

    Add more plots

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def load_data(path="./categorized.csv"):
         categorized = categorized.explode('categories')
     return categorized
 
+data = pd.read_csv("./preprocessed_reviews.csv", index_col=[0])
 categorized = load_data(path="./preprocessed_reviews.csv")
 
 @st.cache_data
@@ -59,8 +60,46 @@ def least_rating_products_by_category(category):
     fig.update_traces(marker_color='red')
     return fig
 
+@st.cache_data
+def review_length_by_category_by_label(category):
+    data = categorized[categorized['categories']==category]
+    fig = px.histogram(data_frame=data, x='review_word_count', 
+                   title=f"Review length distribution by label for {category}", 
+                   color='hypothesis_label', barmode='overlay',
+                   color_discrete_sequence=['blue', 'red'])
+    fig.update_xaxes(range=[0,100])
+    # fig.update_yaxes(range=[0, 10])
+    fig.add_vline(x=data[data['hypothesis_label']=='positive']['review_word_count'].mean(), 
+                line_color='blue', annotation_text='Mean positive', 
+                annotation_textangle = 90)
+    fig.add_vline(x=data[data['hypothesis_label']=='negative']['review_word_count'].mean(), 
+                line_color='red', annotation_text='Mean negative', 
+                annotation_textangle = 90)
+
+    fig.add_vline(x=data[data['hypothesis_label']=='positive']['review_word_count'].median(), 
+                line_color='purple', annotation_text='Median positive', 
+                annotation_textangle = 90)
+    fig.add_vline(x=data[data['hypothesis_label']=='negative']['review_word_count'].median(), 
+              line_color='orange', annotation_text="Median negative", 
+              annotation_textangle = 90, annotation_position='left')
+    return fig
+
+@st.cache_data
+def rating_distribution_of_category_over_time(category):
+    data = categorized[categorized['categories']==category]
+    gb = pd.DataFrame(data.groupby('year')).reset_index()
+    # fig = px.bar(data_frame=gb, x='year', y='rating')
+    return gb
+
+@st.cache_data
+def review_length_average_over_time():
+    v = data.groupby(['hypothesis_label', 'year'])['review_word_count'].mean().reset_index().rename(columns={'hypothesis_label':'sentiment'})
+    fig = px.line(v, x='year', y='review_word_count', color='sentiment', color_discrete_sequence=['red', 'blue'])
+    return fig
+
 
 category: str = st.selectbox(label="Select category of product", options=categorized['categories'].unique())
+st.write(f"Number of product in our dataset for this category - {len(categorized[categorized['categories']==category])}")
 
 col1, col2 = st.columns(2)
 
@@ -83,6 +122,16 @@ with col2:
     fig2 = ratings_by_category(category)
     fig2.update_layout(height=height, width=width)
     st.plotly_chart(fig2, use_container_width=True)
+
+st.write("Please autoscale from top right corner of figure because figure is getting squeezed for some categories")
+fig3 = review_length_by_category_by_label(category)
+fig3.update_layout(height=height, width=width, 
+                   xaxis_title="Number of words in review", yaxis_title="Frequency")
+st.plotly_chart(fig3, use_container_width=True)
+
+fig4 = review_length_average_over_time()
+fig4.update_layout(yaxis_title="Average number of words in review")
+st.plotly_chart(fig4, use_container_width=True)
 
 
 # fig3 = low_rating_categories()
